b1814d86f88bf65c737edfad53c8bf0b5f563b16
Harald Suurorg <harald.suurorg@gmail.com>
2025-04-03 16:53:03 +0300

    Initial

---
+++ resources/js/components/app-sidebar.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { NavFooter } from '@/components/nav-footer';
+import { NavMain } from '@/components/nav-main';
+import { NavUser } from '@/components/nav-user';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { type NavItem, NavButton } from '@/types';
+import { Link } from '@inertiajs/react';
+import { BookOpen, Folder, LayoutGrid, Plus } from 'lucide-react';
+import AppLogo from './app-logo';
+
+const [showCreateCampaign, setShowCreateCampaign] = useState(false);
+
+const mainNavItems: NavButton[] = [
+    {
+        title: 'Create a campaign',
+        icon: Plus,
+        onClick: () => setShowCreateCampaign(true),
+    },
+];
+
+const footerNavItems: NavItem[] = [
+    // {
+    //     title: 'Repository',
+    //     href: 'https://github.com/laravel/react-starter-kit',
+    //     icon: Folder,
+    // },
+    // {
+    //     title: 'Documentation',
+    //     href: 'https://laravel.com/docs/starter-kits',
+    //     icon: BookOpen,
+    // },
+];
+
+export function AppSidebar() {
+    return (
+        <Sidebar collapsible="icon" variant="inset">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <Link href="/dashboard" prefetch>
+                                <AppLogo />
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+
+            <SidebarContent>
+                <NavMain items={mainNavItems} />
+            </SidebarContent>
+
+            <SidebarFooter>
+                <NavFooter items={footerNavItems} className="mt-auto" />
+                <NavUser />
+            </SidebarFooter>
+        </Sidebar>
+    );
+}
---
+++ resources/js/components/nav-main.tsx
@@ -0,0 +1,40 @@
+import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { type NavItem, NavButton } from '@/types';
+import { Link, usePage } from '@inertiajs/react';
+
+function isNavItem(item: NavItem | NavButton): item is NavItem {
+    return 'href' in item;
+}
+
+export function NavMain({ items = [] }: { items: NavItem[] | NavButton[] }) {
+    const page = usePage();
+    return (
+        <SidebarGroup className="px-2 py-0">
+            <SidebarGroupLabel>Platform</SidebarGroupLabel>
+            <SidebarMenu>
+                {items.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                        {isNavItem(item) ? (
+                            <SidebarMenuButton
+                                asChild isActive={item.href === page.url}
+                                tooltip={{ children: item.title }}
+                            >
+                                <Link href={item.href} prefetch>
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        ) : (
+                            <SidebarMenuButton
+                                tooltip={{ children: item.title }}
+                            >
+                                {item.icon && <item.icon />}
+                                <span>{item.title}</span>
+                            </SidebarMenuButton>
+                        )}
+                    </SidebarMenuItem>
+                ))}
+            </SidebarMenu>
+        </SidebarGroup>
+    );
+}
