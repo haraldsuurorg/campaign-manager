c5b5ecb88295c78c7202ce284d16a5f217af7bea
Harald Suurorg <harald.suurorg@gmail.com>
2025-04-04 16:40:53 +0300

    Campaign form complete, before creating the campaign model and migration

---
+++ resources/js/components/create-campaign-form.tsx
@@ -0,0 +1,154 @@
+import { boolean, number, z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from './ui/form';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { Switch } from './ui/switch';
+
+const formSchema = z.object({
+    title: z.string({
+        message: 'Please enter a title'
+    }).min(1, {
+        message: 'Please enter a title'
+    }).max(50, {
+        message: 'Title must be less than 50 characters'
+    }),
+    status: z.boolean(),
+    payoutEstonia: z.number().positive().optional().nullable(),
+    payoutSpain: z.number().positive().optional().nullable(),
+    payoutBulgaria: z.number().positive().optional().nullable(),
+}).refine((data) => {
+    return data.payoutEstonia !== null && data.payoutEstonia !== undefined ||
+           data.payoutSpain !== null && data.payoutSpain !== undefined ||
+           data.payoutBulgaria !== null && data.payoutBulgaria !== undefined;
+}, {
+    message: 'At least one Payout field must be filled',
+    path: ['payoutEstonia']
+})
+
+export function CampaignCreationForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: '',
+            status: false,
+            payoutEstonia: null,
+            payoutBulgaria: null,
+            payoutSpain: null,
+        }
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+                <FormField
+                    control = {form.control}
+                    name = 'title'
+                    render = {({ field }) => (
+                        <FormItem>
+                            <FormLabel>Title</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name='status'
+                    render = {({ field }) => (
+                        <FormItem>
+                            <FormLabel>Activity status</FormLabel>
+                            <FormControl>
+                                <Switch
+                                    id='activity-status'
+                                    checked={field.value}
+                                    onCheckedChange={field.onChange}
+                                />
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name='payoutEstonia'
+                    render = {({ field }) => (
+                        <FormItem>
+                            <FormLabel>Payout for Estonia 🇪🇪</FormLabel>
+                            <FormControl>
+                                <Input
+                                    type='number'
+                                    {...field}
+                                    value={field.value ?? ''}
+                                    onChange={e => {
+                                        const value = e.target.value;
+                                        field.onChange(value === '' ? null : Number(value));
+                                    }} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name='payoutSpain'
+                    render = {({ field }) => (
+                        <FormItem>
+                            <FormLabel>Payout for Spain 🇪🇸</FormLabel>
+                            <FormControl>
+                                <Input
+                                    type='number'
+                                    {...field}
+                                    value={field.value ?? ''}
+                                    onChange={e => {
+                                        const value = e.target.value;
+                                        field.onChange(value === '' ? null : Number(value));
+                                    }} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name='payoutBulgaria'
+                    render = {({ field }) => (
+                        <FormItem>
+                            <FormLabel>Payout for Bulgaria 🇧🇬</FormLabel>
+                            <FormControl>
+                                <Input
+                                    type='number'
+                                    {...field}
+                                    value={field.value ?? ''}
+                                    onChange={e => {
+                                        const value = e.target.value;
+                                        field.onChange(value === '' ? null : Number(value));
+                                    }} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button
+                    type='submit'
+                    className='flex justify-self-end'
+                >
+                    Create
+                </Button>
+            </form>
+        </Form>
+    )
+}
--- resources/js/components/create-campaign.tsx
+++ resources/js/components/create-campaign.tsx
@@ -1,7 +1,15 @@
-import { Description } from "@radix-ui/react-dialog";
-import { AlertDialog, AlertDialogContent, AlertDialogTitle, AlertDialogTrigger, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogCancel } from "./ui/alert-dialog";
-import { Button } from "./ui/button";
-import { AlertDescription } from "./ui/alert";
+import {
+    AlertDialog,
+    AlertDialogContent,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogCancel } from './ui/alert-dialog';
+import { Button } from './ui/button';
+import { CampaignCreationForm } from './create-campaign-form';
+import { X } from 'lucide-react';
 
 export function CreateCampaign() {
     return (
@@ -10,22 +18,14 @@ export function CreateCampaign() {
                 <Button>Create a campaign</Button>
             </AlertDialogTrigger>
             <AlertDialogContent>
+                <AlertDialogCancel className='border-none w-fit absolute right-3 top-4.5 shadow-none'>
+                    <X className='!h-6 !w-6'/>
+                </AlertDialogCancel>
                 <AlertDialogHeader>
-                    <AlertDialogTitle>Create a new campaign</AlertDialogTitle>
-                    <AlertDescription>Please fill in the fields below</AlertDescription>
+                    <AlertDialogTitle className='pb-2'>Create a new campaign</AlertDialogTitle>
+                    <AlertDialogDescription className='font-medium'>Please add a title for Your campaign. Activating the campaign is optional at this stage, You can do it later as well. Please also include at least one payout for Your desired Country. If a payout field for a country is left empty, the campaign will not be published in that country.</AlertDialogDescription>
                 </AlertDialogHeader>
-                <div>
-                    <form>
-                        <label>Title</label>
-                        <input
-                            placeholder="asd"
-                        >
-                        </input>
-                    </form>
-                </div>
-                <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                </AlertDialogFooter>
+                <CampaignCreationForm />
             </AlertDialogContent>
         </AlertDialog>
     );
